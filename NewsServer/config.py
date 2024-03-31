e31268ce3cba9645e825d54dbbb62464fc4c2c7b
Aditya bhardwaj <bhardwajaditya394@gmail.com>
2024-03-31 21:27:46 +0530

    added service

--- NewsServer/config.py
+++ NewsServer/config.py
@@ -1,7 +1,7 @@
 import asyncio
 KAFKA_BOOTSTRAP_SERVERS="localhost:9092"
 KAFKA_TOPIC="news-data"
-SERVER_EMAIL="bhardwajaditya394@gmail.com"
-RECEIVER_EMAIL="12113012@nitkkr.ac.in"
-SERVER_PASSWORD="suln csms gjxt fugr"
+SERVER_EMAIL="password"
+RECEIVER_EMAIL="email"
+SERVER_PASSWORD="password"
 loop=asyncio.get_event_loop()
