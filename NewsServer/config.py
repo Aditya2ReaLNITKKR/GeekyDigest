1556a7333373d4ce7aea1bff228f335bc0782d21
Aditya bhardwaj <bhardwajaditya394@gmail.com>
2024-03-31 21:25:10 +0530

    added email service

--- NewsServer/config.py
+++ NewsServer/config.py
@@ -1,6 +1,7 @@
 import asyncio
 KAFKA_BOOTSTRAP_SERVERS="localhost:9092"
 KAFKA_TOPIC="news-data"
-SERVER_EMAIL=""
-SERVER_PASSWORD=""
+SERVER_EMAIL="bhardwajaditya394@gmail.com"
+RECEIVER_EMAIL="12113012@nitkkr.ac.in"
+SERVER_PASSWORD="suln csms gjxt fugr"
 loop=asyncio.get_event_loop()
--- NewsServer/notifier.py
+++ NewsServer/notifier.py
@@ -1,7 +1,9 @@
 from aiokafka import AIOKafkaConsumer,AIOKafkaProducer
 import json
-from NewsServer.config import KAFKA_TOPIC,KAFKA_BOOTSTRAP_SERVERS,loop
+from NewsServer.config import KAFKA_TOPIC,KAFKA_BOOTSTRAP_SERVERS,loop,SERVER_EMAIL,SERVER_PASSWORD,RECEIVER_EMAIL
 import asyncio
+import smtplib,ssl
+from email.message import EmailMessage
 async def consume():
     consumer=AIOKafkaConsumer(
         KAFKA_TOPIC,
@@ -11,7 +13,7 @@ async def consume():
     await consumer.start()
     try:
         async for msg in consumer:
-            print("consumed :")
+            await email(json.loads(msg.value))
     finally:
         await consumer.stop()
 
@@ -22,3 +24,19 @@ async def produce(msg):
         await producer.send(KAFKA_TOPIC,json.dumps(msg).encode('utf-8'))
     finally:
         await producer.stop()
+
+async def email(msg):
+    print(str(msg[0][0]))
+    body=f"Heading: {msg[0][0][0]} \n\n{msg[0][0][1]}"
+    em=EmailMessage()
+    em['From']=SERVER_EMAIL
+    em['To']=RECEIVER_EMAIL
+    em['subject']="Don't forget to subscribe"
+    em.set_content(body)
+    context=ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+        smtp.login(SERVER_EMAIL,SERVER_PASSWORD)
+        smtp.sendmail(SERVER_EMAIL,RECEIVER_EMAIL,em.as_string())
+
+
+
