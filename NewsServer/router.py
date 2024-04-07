a31b87b665e955655f74ec3d00bce66e6fb2bac8
Aditya bhardwaj <bhardwajaditya394@gmail.com>
2024-04-07 17:48:18 +0530

    added mysql to store email and update check and time schedular

--- NewsServer/router.py
+++ NewsServer/router.py
@@ -1,14 +1,42 @@
 from fastapi import APIRouter
 from NewsServer.scrap import main
 import aioredis
+from typing import List
 import asyncio
 import json
+from fastapi import Depends,HTTPException
+from sqlalchemy.orm import Session
+from NewsServer import utils,models,schemas
+from NewsServer.database import SessionLocal,engine
+models.Base.metadata.create_all(bind=engine)
 company_route=APIRouter()
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 redis=aioredis.Redis(host="localhost",port=6379,db=0)
 @company_route.get('/news')
 async def news():
     content=await redis.get('news-data')
     res=json.loads(content)
-    print(res)
+    
     return res
-    # return 'hello world'
+
+@company_route.post('/users/',response_model=schemas.User)
+def create_user(user:schemas.UserBase,db:Session=Depends(get_db)):
+    db_user=utils.get_user_by_email(db,email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400,detail="email already exist")
+    return utils.create_user(db,user=user)
+
+@company_route.get('/users/',response_model=List[schemas.User])
+def read_user(skip:int=0,limit:int=100,db:Session=Depends(get_db)):
+    users=utils.get_users(db,skip=skip,limit=limit)
+    return users
+
+
+
+
--- NewsServer/scrap.py
+++ NewsServer/scrap.py
@@ -7,49 +7,112 @@ from NewsServer.notifier import produce
 import aioredis
 redis=aioredis.Redis(host="localhost",port=6379,db=0)
 
-urls=['https://www.jpmorgan.com/technology/technology-blog']
-
-async def parse(content):
-        soup= BeautifulSoup(content,'lxml')
-        blocks=soup.find_all('div',class_='text cmp-text--light cmp-text--pt aem-GridColumn aem-GridColumn--default--12')
-        res=[]
-        for block in blocks:
-            content=block.find('p').text
-            heading=block.find('span').text
-            # print(heading)
-            res.append([heading,content])
-        return res
-async def fetch(session,url):
-    async with session.get(url) as request:
+# urls=['https://www.jpmorgan.com/technology/technology-blog','https://www.intuit.com/blog/category/innovative-thinking/tech-innovation/','https://techcommunity.microsoft.com/t5/custom/page/page-id/Blogs']
+async def parse_intuit(session):
+      url= 'https://www.intuit.com/blog/category/innovative-thinking/tech-innovation/'
+      async with session.get(url) as request:
         if request.status!=200:
              raise Exception(f"Failed to fetch {url}: {request.status}")
         
         content=await request.text()
-        # print(content)
-        res=await parse(content)
+       
+        res=[]
+        soup=BeautifulSoup(content,'lxml')
+        blocks=soup.find_all('a',class_='rkv-card__link')
+        for block in blocks:
+            content=block.get('aria-label')
+            link=block.get('href')
+            dict={
+                  "link":link,
+                  "title":'Innovative Thinking',
+                 "body":content,
+            }
+            res.append(dict)
         return res
-
-
-async def fetch_all(session,urls):
+async def parse_jpmorgan(session):
+        url= 'https://www.jpmorgan.com/technology/technology-blog'
+        async with session.get(url) as request:
+            if request.status!=200:
+                raise Exception(f"Failed to fetch {url}: {request.status}")
+            
+            content=await request.text()
+        res=[]
+        soup= BeautifulSoup(content,'lxml')
+        blocks=soup.find_all('div',class_='cmp-text')
+        for block in blocks:
+             title_name=''
+             title=block.find('span',class_='title-large')
+             if title:
+                  title_name=title.text
+             body_tag=block.find('p')
+             body=''
+             if body_tag:
+                  body=block.find('p').text
+             a_tag=block.find('a',class_='chaseanalytics-track-link')
+             link='https://www.jpmorgan.com'
+             if a_tag:
+                  link= block.find('a',class_='chaseanalytics-track-link').get('href')
+                  link='https://www.jpmorgan.com'+link
+             dict={
+                  "link":link,
+                  "title":title_name,
+                  "body":body,
+             }
+             res.append(dict)
+        return res
+async def parse_microsoft(session):
+        url= 'https://techcommunity.microsoft.com/t5/custom/page/page-id/Blogs'
+        async with session.get(url) as request:
+            if request.status!=200:
+                raise Exception(f"Failed to fetch {url}: {request.status}")
+            
+            content=await request.text()
+        res=[]
+        soup= BeautifulSoup(content,'lxml')
+        blocks=soup.find_all('div',class_='item')
+        for block in blocks:
+             link=block.find('div',class_='blog-title').find('a').get('href')
+             title=block.find('span',class_='blog-name').find('a').get_text(strip=True)
+             body=block.find('div',class_='details').find('div',class_='body').get_text(strip=True)
+             dict={
+                  "link":'https://techcommunity.microsoft.com'+link,
+                  "title":title,
+                  "body":body
+             }
+             res.append(dict)
+      
+        return res
+async def fetch_all(session):
     tasks=[]
-    for url in urls:
-        task=asyncio.create_task(fetch(session,url))
-        tasks.append(task)
+    # asyncio.create_task(fetch(session,url))
+    tasks.append(asyncio.create_task(parse_jpmorgan(session)))
+    tasks.append(asyncio.create_task(parse_intuit(session)))
+    tasks.append(asyncio.create_task(parse_microsoft(session)))
     res=await asyncio.gather(*tasks)
     return res
 
-async def check_update():
+async def check_update(res):
     content=await redis.get('news-data')
-    res=json.loads(content)
-    # print(res)
-    await produce(res)
-          
+    if content is None:
+         await redis.set('news-data',json.dumps(res))
+    
+    else:
+         prev_res=json.loads(content)
+     #     print(prev_res[0][1]['title'])
+     #     print(res[0][1]['title'])
+         if prev_res[0][1]['title'] != res[0][1]['title'] or prev_res[1][0]['title'] != res[1][0]['title'] or prev_res[2][0]['title'] != res[2][0]['title']:
+              print(' change')
+              await redis.set('news-data',json.dumps(res))
+              await produce(res)
+
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        res=await fetch_all(session,urls)
-        await redis.set('news-data',json.dumps(res))
-        await check_update()
+        res=await fetch_all(session)
+        await check_update(res)
+     #    print(res)
+     # #    return res
+     #    await redis.set('news-data',json.dumps(res))
         
 # asyncio.run(main())
 
